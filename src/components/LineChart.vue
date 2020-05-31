cd9f1e19cff2e542c79151b97bfd27443aec65a7
kawa1228 <sxa.1105bo@gmail.com>
2020-05-31 12:36:50 +0900

    Line chartを作成

---
+++ src/components/LineChart.vue
@@ -0,0 +1,43 @@
+<script>
+import { Line } from 'vue-chartjs';
+
+const setDegreeCelsius = (value, index, values) => {
+  console.log(value, index, values)
+  return `${value}℃`
+}
+
+export default {
+  extends: Line,
+  name: 'LineChart',
+  data:() => ({
+    chartdata: {
+      labels: ["12:00", "15:00", "18:00", "21:00", "0:00", "3:00", "6:00", "9:00"],
+      datasets: [
+        {
+          label: 'temperature',
+          data: [23, 25, 22, 20, 19, 19, 19, 20, 23],
+          borderColor: 'rgba(255, 146, 51, 1)',
+          backgroundColor: 'rgba(255, 146, 51, 0.2)',
+        }
+      ]
+    },
+    options: {
+      scales: {
+          yAxes: [{
+              ticks: {
+                  beginAtZero: true,
+                  callback: function(value, index, values) {
+                      return setDegreeCelsius(value, index, values);
+                  }
+              }
+          }]
+      },
+      responsive: true,
+      maintainAspectRatio: false
+    }
+  }),
+  mounted () {
+    this.renderChart(this.chartdata, this.options);
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <LineChart/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import LineChart from "@/components/LineChart.vue";
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    LineChart
+  },
+  mounted() {
+    console.log('mounted')
   }
 }
 </script>
